1dd921efd1f664a56ffb3b2ed2fe1194a79d32ef
suhyun9764 <suhyun9764@naver.com>
2024-03-10 22:26:43 +0900

    build(): innit commit

---
+++ src/main/java/com/sparta/sparta_lecture_server/SpartaLectureServerApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.sparta_lecture_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpartaLectureServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpartaLectureServerApplication.class, args);
+    }
+
+}
